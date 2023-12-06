c1ea4edc0a7fa516567a265ba0562817d8f2e893
Eliasnew52 <xxeliasnew52xx@gmail.com>
2023-12-05 21:50:21 -0600

    Login - Index with Menu Done - Admin Access Done, Req Almost Done

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    #path("register", views.register_view, name="register"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,65 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.utils.datastructures import MultiValueDictKeyError
+from django.contrib import messages
 
-# Create your views here.
+
+#Models Importation
+from .models import (Topping, Menu_Item, Profile,
+Extras, Order, OrderItem,  User, News, )
+
+import datetime
+
+
+@login_required()
 def index(request):
-    return HttpResponse("Project 3: TODO")
+
+    if not request.user.is_authenticated:
+        return render(request, "login.html", {"message": None})
+
+    Menu = Menu_Item.objects.exclude(category__icontains="Topping").exclude(category__icontains="Extra")
+
+    Orders = Order.objects.filter(owner=request.user.profile, is_ordered=False)
+    Cart = []
+
+    item_count = 0
+
+    if Orders.exists():
+        user_order = Orders[0]
+        user_order_items = user_order.ordered_items.all()
+        current_order_products = [menu_item.menu_item for menu_item in user_order_items]
+
+        #toma el numero de items en el carrito
+        item_count = user_order.ordered_items.count()
+
+    context ={
+
+        'cart': Cart,
+        'item_count': item_count,
+        "user": request.user,
+        "menu_item": Menu.order_by('-category')
+    }
+    return render(request, "index.html", context)
+
+
+# USER LOGIN VIEW
+def login_view(request):
+
+    if request.method == "GET":
+
+        return render(request,"login.html")
+
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "login.html", {"message": "Datos invalidos."})
